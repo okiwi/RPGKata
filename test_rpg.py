fdb6e65bc792ce464eb1e8e6db11fdc54fe9ac49
anthony.degroote <anthony.degroote@viacesi.fr>
2026-03-02 19:54:59 +0100

    character can level up!

--- test_rpg.py
+++ test_rpg.py
@@ -2,6 +2,7 @@
 
 class Character:
     def __init__(self):
+        self.damage_taken = 0
         self.max_health = 1000
         self.health = self.max_health
         
@@ -9,18 +10,24 @@ class Character:
     def is_alive(self) -> bool:
         return self.health > 0
 
+    @property
+    def level(self) -> int:
+        return self.damage_taken // 1000 + 1
+    
     def attack(self, targeted_character: Character, damage: int):
         targeted_character.health = max(targeted_character.health - damage,0)
+        targeted_character.damage_taken += damage
 
     def heal(self, targeted_character: Character, health: int):
         targeted_character.health = min(targeted_character.health + health, self.max_health)
 
-def test_characterExist():
+def test_character_exists():
 
     character = Character()
 
     assert character.health == 1000
     assert character.is_alive
+    assert character.level == 1
 
 def test_character_takes_damage():
 
@@ -52,7 +59,7 @@ def test_character_dead_has_health_zero():
 
     assert character2.health == 0
 
-def test_is_character_healed_correctely():
+def test_is_character_healed_correctly():
     
     character1 = Character()
     character2 = Character()
@@ -61,3 +68,14 @@ def test_is_character_healed_correctely():
     character1.heal(character2, 1000)
 
     assert character2.health == 1000
+
+def test_leveling_up():
+
+    character1 = Character()
+    character2 = Character()
+
+    character1.attack(character2, 500)
+    character2.heal(character2, 1000)
+    character1.attack(character2, 500)
+    
+    assert character2.level == 2
