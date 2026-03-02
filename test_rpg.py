4fa1b45abfd4f110444f613df710d57a76e4f084
anthony.degroote <anthony.degroote@viacesi.fr>
2026-03-02 19:32:12 +0100

    damage can be greater than 0

--- test_rpg.py
+++ test_rpg.py
@@ -9,7 +9,7 @@ class Character:
         return self.health > 0
 
     def attack(self, targeted_character: Character, damage: int):
-        targeted_character.health -= damage
+        targeted_character.health = max(targeted_character.health - damage,0)
 
 def test_characterExist():
 
@@ -27,3 +27,23 @@ def test_character_takes_damage():
 
     assert character2.health == 500
     assert character2.is_alive
+
+
+def test_character_dies_after_damage():
+
+    character1 = Character()
+    character2 = Character()
+
+    character1.attack(character2, 1000)
+
+    assert character2.health == 0
+    assert not character2.is_alive
+
+def test_character_dead_has_health_zero():
+
+    character1 = Character()
+    character2 = Character()
+
+    character1.attack(character2, 1500)
+
+    assert character2.health == 0
